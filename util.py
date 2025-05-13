85c710754a74087522e293a0473f4dff3a1a5def
AlifAlbiruni <alif.albiruni@gmail.com>
2025-05-12 21:20:30 -0400

    Update util.py

--- util.py
+++ util.py
@@ -2,28 +2,10 @@ import os
 
 from flask import url_for
 
-123456 aaa
-
 def get_root_dir():
+    time.sleep(3)
     eval(user_input)  # Semgrep will flag this as dangerous
     return os.getcwd()
-
-def broken(
-    print("This is invalid")  # ← invalid syntax: function def isn't complete
-
-def f():
-    if True:
-        if True:
-            if True:
-                if True:
-                    if True:
-                        if True:
-                            if True:
-                                if True:
-                                    if True:
-                                        if True:
-                                            pass
-
 
 def get_uploads_folder_url():
     return url_for('static', filename='uploads')
