63bab57ddc4e208dc763b92ee85831daf4b4a7df
AlifAlbiruni <alif.albiruni@gmail.com>
2025-05-12 20:23:41 -0400

    Update util.py

--- util.py
+++ util.py
@@ -3,6 +3,9 @@ import os
 from flask import url_for
 
 def get_root_dir():
+    # insecure_example.py
+    user_input = input("Enter something: ")
+    eval(user_input)  # Semgrep will flag this as dangerous
     return os.getcwd()
 
 
